0be090273c66b743ccca35a4ec683f877e7ed594
lea-beaulieu <87803489+lea-beaulieu@users.noreply.github.com>
2021-10-10 16:28:03 +0200

    set the launching of the boxes + comments

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
+// Parameters A. Setting Random 
 function rand(from = 0, to = 1) {
 
     return Math.floor(from + Math.random() * (to - from));
 }
 
+
+// Parameters B. Scoring
 let pts = 0;
 
 
-/*for (let i = 0; i < 3; i++) {
-    launchRedBox()[i];
+// THE GAME LOGIC
 
-} */
-
-
-
-// Step 1 Launch Red Boxes (3 boxes each sec during 10 secondes)
+// (CONFIG) Counting occurences of sequence (10 sequences in total during the game)
 let counterBoxSequence = 1;
 
-const BoxSequence = function() {
 
+// (CONFIG) Defining the BoxSequence launch, i.e. 3 Red boxes + 1 Blue box each 2 seconds during 10 seconds
+const BoxSequence = function() {
 
     launchRedBox();
     launchRedBox();
@@ -32,50 +31,58 @@ const BoxSequence = function() {
     }
 };
 
-let timeoutId = setTimeout(BoxSequence, 1000);
+// (DIRECT IMPACT ON THE GAME) Launching the boxes with Box Sequence
+BoxSequence()
 
 
-l
 
+// (CONFIG) Defining property of ONE RED box launch
 function launchRedBox() {
 
-
+    // Create a NEW div with the class 'redbox', each red box is separated in a proper div
     let $div = document.createElement('div');
     $div.classList.add('redbox');
+    // Setting a random position where the box will appear
     let left = rand(0, window.innerWidth - 100);
     let top = rand(0, window.innerHeight - 100);
     $div.style.left = `${left}px`;
     $div.style.top = `${top}px`;
 
+    // Adding this NEW div within the "tab" section 
+    document.getElementsByTagName('table')[0].appendChild($div);
 
-
-    document.getElementsByTagName('body')[0].appendChild($div);
-
-
+    // Setting the actions WHEN clicking on the red box
     $div.onclick = function() {
-        document.getElementsByTagName('body')[0].removeChild($div);
+        // Removing the red box
+        document.getElementsByTagName('table')[0].removeChild($div);
+        // Updating the score (+1pts for each red box)
         document.querySelector('#score span').innerText = ++pts;
     }
 
 }
 
+// (CONFIG) Defining property of ONE BLUE box launch
 function launchBlueBox() {
 
 
+    // Create a NEW div with the class 'bluebox', each blue box is separated in a proper div
     let $div = document.createElement('div');
     $div.classList.add('bluebox');
+    // Setting a random position where the box will appear
     let left = rand(0, window.innerWidth - 100);
     let top = rand(0, window.innerHeight - 100);
     $div.style.left = `${left}px`;
     $div.style.top = `${top}px`;
 
 
+    // Adding this NEW div within the "tab" section 
+    document.getElementsByTagName('table')[0].appendChild($div);
 
-    document.getElementsByTagName('body')[0].appendChild($div);
 
-
+    // Stopping the game WHEN clicking on a blue box
     $div.onclick = function() {
         alert("you lose");
+        location.replace("defeat.html");
     }
 
 }
